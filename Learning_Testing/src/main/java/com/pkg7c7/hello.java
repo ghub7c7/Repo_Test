da5b317793cc9b1447dbf9826726f7c22b44649a
mai <mai@stclp7c7>
2023-10-11 20:39:06 +0300

    Commit1

--- Learning_Testing/src/main/java/com/pkg7c7/hello.java
+++ Learning_Testing/src/main/java/com/pkg7c7/hello.java
@@ -3,7 +3,9 @@ package com.pkg7c7;
 public class hello
 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		System.out.println("Learning and Testing");
 		System.out.println("hello world");
 
 	}
